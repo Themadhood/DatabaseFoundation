b62491ae134850f18d1c6e64ebb26dabdd99c688
Themadhood's Codes <148127209+Themadhood@users.noreply.github.com>
2024-04-02 16:21:52 -0500

    Add files via upload

---
+++ Database/#py creation.py
@@ -0,0 +1,61 @@
+__Program__     = "Database"    
+__programer__   = "Themadhood Pequot"
+__Date__        = "4/2/2024"
+__version__     = "0.0.1"
+__update__      = ""
+__info__        = ""   
+
+import Error
+
+st = Error.time.time()
+Error.LenTime(st)
+Error.Log(f"","Log.txt")
+
+
+
+def NewRecord(table):
+    return table[0].copy()
+
+def NewTable():
+    "[{defalt record},{}]"
+    return [{},{}]
+
+
+
+
+    
+tables = {"Table 1":NewTable()}
+
+
+
+
+try:
+    pass
+
+except Exception as e:
+    if self._Error:
+        raise
+    Error.UploadError([__Program__,__version__,"class","def",
+                                f"mesage",e],"Functions")
+
+
+
+
+if __name__ == "__main__":
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
